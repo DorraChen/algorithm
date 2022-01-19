bfc57ca46af6af8dbc4484227af4921b62da2c7f
dorra <1814786186@qq.com>
2022-01-19 18:06:23 +0800

    algorithm

---
+++ src/main/java/com/dorra/algorithm/leetcode/listnode/ListNode.java
@@ -0,0 +1,23 @@
+package com.dorra.algorithm.leetcode.listnode;
+
+/**
+ * @author dorra
+ * @date 2022/1/19 17:41
+ * @description Definition for singly-linked list.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
