6efeaace397563220a4fd64c534e7675ec5de54c
dorra <1814786186@qq.com>
2022-01-20 18:03:52 +0800

    algorithm

--- src/main/java/com/dorra/algorithm/leetcode/listnode/reverse_linked_list/v1/Solution.java
+++ src/main/java/com/dorra/algorithm/leetcode/listnode/reverse_linked_list/v1/Solution.java
@@ -4,7 +4,7 @@ import com.dorra.algorithm.leetcode.listnode.ListNode;
 
 /**
  * @author dorra
- * @date 2021/10/21 19:14
+ * @date 2022/1/20 13:14
  * @description LeetCode 206: https://leetcode-cn.com/problems/reverse-linked-list/
  * 反转链表:方法一 迭代
  */
@@ -12,21 +12,26 @@ public class Solution {
 
     /**
      * 解析: 相当于把链表的指针反转过来
+     * <p>
+     * <img src="../Reverse Linked List v1.png" />
+     *
      * @param head
      * @return
      */
     public ListNode reverseList(ListNode head) {
-        // 定义头指针
+        // 定义第一个指针
         ListNode prev = null;
         // 当前指针
         ListNode curr = head;
         // curr 为 null 说明到了最后一个元素
         while (curr != null) {
-            // 为了防止指针断掉,所以要先记录curr的下一个指针
+            // 为了防止链表断掉,所以要先记录curr的下一个指针
             ListNode next = curr.next;
-            // curr下一个指针设置为prev指针
+            // curr的指针反转,指向prev
             curr.next = prev;
+            // prev移动到curr位置
             prev = curr;
+            // curr移动到next位置
             curr = next;
         }
         return prev;
---
+++ src/main/java/com/dorra/algorithm/leetcode/listnode/reverse_linked_list/v2/Solution.java
@@ -0,0 +1,33 @@
+package com.dorra.algorithm.leetcode.listnode.reverse_linked_list.v2;
+
+import com.dorra.algorithm.leetcode.listnode.ListNode;
+
+/**
+ * @author dorra
+ * @date 2022/1/20 14:14
+ * @description LeetCode 206: https://leetcode-cn.com/problems/reverse-linked-list/
+ * 反转链表:方法二 递归
+ */
+public class Solution {
+
+    /**
+     * 可以把问题看成, 反转头节点以及头结点剩下的节点
+     *
+     * <img src="../Reverse Linked List v2.png" />
+     *
+     * <img src="../Reverse Linked List v2-2.png" />
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        // 递归终止条件
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
